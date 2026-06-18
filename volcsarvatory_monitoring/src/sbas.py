1171cfc5f16b3f56ddc426dd5b22d21c234f480f
mfangaritav <mf.angarita52@gmail.com>
2026-06-18 15:07:52 -0800

    remove cast of season

--- volcsarvatory_monitoring/src/sbas.py
+++ volcsarvatory_monitoring/src/sbas.py
@@ -358,8 +358,6 @@ def get_sbas_pairs(
     if bridge is None:
         bridge = 1
 
-    if isinstance(season, list):
-        season = tuple(season)
     if isinstance(season, tuple):
         pairs = build_sbas_pairs_default(dic, start, season, tbaseline, target, bridge)
     elif isinstance(season, dict):
